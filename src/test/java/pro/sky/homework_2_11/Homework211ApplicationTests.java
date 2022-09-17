fabcef5cff9c5395c492c208e6fcc335b2418c42
Sasha <slanecru@outlook.com>
2022-09-18 01:04:58 +0300

    Initial commit

---
+++ src/test/java/pro/sky/homework_2_11/Homework211ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.homework_2_11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework211ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
